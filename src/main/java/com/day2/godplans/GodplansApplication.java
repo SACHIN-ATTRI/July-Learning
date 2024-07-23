6ab00506dca97025dacc5593b09aed29ab3c19fb
SACHIN CHAUDHARY <116663395+SACHIN-ATTRI@users.noreply.github.com>
2024-07-24 00:59:12 +0530

    intialize the spring.io to cloud added index.html

---
+++ src/main/java/com/day2/godplans/GodplansApplication.java
@@ -0,0 +1,13 @@
+package com.day2.godplans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodplansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodplansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/day2/godplans/GodplansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.day2.godplans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodplansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
